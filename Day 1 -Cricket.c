8347d67e5243f7564bec608b50e672630a883947
Rudra-0856 <rpsr7746@gmail.com>
2024-09-29 12:35:11 +0530

    Add files via upload

---
+++ Day 1 -Cricket.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+    scanf("%d", &marks);
+    if (marks >= 12)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
